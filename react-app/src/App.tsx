494d94493f4847fda1a165709cb2462f0b860805
David Pilat <dpilat13@gmail.com>
2023-04-12 20:06:42 -0700

    Managing Componetn State Exercieses

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -7,6 +7,7 @@ import ListGroup from "./components/ListGroup";
 import produce from "immer";
 import NavBar from "./components/NavBar";
 import Cart from "./components/Cart";
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
   const items = ["New York", "Los Angeles", "San Francisco"];
@@ -15,14 +16,40 @@ function App() {
     { id: 1, title: "Bug 1", fixed: false },
     { id: 2, title: "Bug 2", fixed: false },
   ]);
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: "John",
+    },
+  });
 
+  const [pizza, setPizza] = useState({
+    name: "Spicy Pepperoni",
+    toppings: ["Mushroom"],
+  });
+
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, tittle: "Product 1", quantity: 1 },
+      { id: 1, tittle: "Product 1", quantity: 1 },
+    ],
+  });
   const handleClick = () => {
-    setBugs(
+    /*setBugs(
       produce((draft) => {
         const bug = draft.find((bug) => bug.id === 1);
         if (bug) bug.fixed = true;
       })
-    );
+    );*/
+    //setGame({ ...game, player: { ...game.player, name: "Bob" } });
+    setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
   };
 
   /*<ListGroup
@@ -37,12 +64,28 @@ function App() {
       ))}
       <Button color="primary" onClick={handleClick}>
         Click me
-      </Button> */
+      </Button> 
+      
+      
+      <NavBar cartItemsCount={cartItems.length} />
+      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
+      */
 
   return (
     <div>
-      <NavBar cartItemsCount={cartItems.length} />
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
+      <ExpandableText maxChars={10}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni
+        praesentium delectus recusandae earum expedita error ea veniam quo eius,
+        ut, vel amet, repellat sapiente? Voluptatibus animi eligendi ab ex.
+        Suscipit ipsa omnis quod enim, ipsam ratione quam libero? Ullam possimus
+        id asperiores aliquid nobis! Quas assumenda fugiat minima amet
+        exercitationem officiis dolores iure voluptas nulla consequatur
+        similique dicta architecto ipsam velit vel, ab modi obcaecati excepturi?
+        Reprehenderit, quis et porro maxime qui, distinctio ipsum harum fugiat
+        dolor eveniet rem dicta voluptatibus suscipit nemo aliquid tempore
+        pariatur praesentium exercitationem illum unde quam sit deserunt
+        nostrum! Soluta ab sed provident facilis quo?
+      </ExpandableText>
     </div>
   );
 }
