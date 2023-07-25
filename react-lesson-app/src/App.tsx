0c088b7cb5c660bd5dede98568016044f3362348
David Pilat <dpilat13@gmail.com>
2023-07-25 16:26:15 -0700

    Connecting to Tha Backend examples

--- react-lesson-app/src/App.tsx
+++ react-lesson-app/src/App.tsx
@@ -1,80 +1,78 @@
 import "./App.css";
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button/Button";
-import Like from "./components/Like";
-import ListGroup from "./components/ListGroup";
-import produce from "immer";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
-import ExpandableText from "./components/ExpandableText";
-import Form from "./components/Form";
+import { useState, useEffect } from "react";
+import apiClient, { CanceledError } from "./services/api-client";
+import userService, { User } from "./services/user-service";
+import useUsers from "./hooks/useUsers";
 
 function App() {
-  const items = ["New York", "Los Angeles", "San Francisco"];
-  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
-  const [bugs, setBugs] = useState([
-    { id: 1, title: "Bug 1", fixed: false },
-    { id: 2, title: "Bug 2", fixed: false },
-  ]);
-  const [game, setGame] = useState({
-    id: 1,
-    player: {
-      name: "John",
-    },
-  });
+  const { users, error, isLoading, setUsers, setError } = useUsers();
 
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushroom"],
-  });
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
 
-  const [cart, setCart] = useState({
-    discount: 0.1,
-    items: [
-      { id: 1, tittle: "Product 1", quantity: 1 },
-      { id: 1, tittle: "Product 1", quantity: 1 },
-    ],
-  });
-  const handleClick = () => {
-    /*setBugs(
-      produce((draft) => {
-        const bug = draft.find((bug) => bug.id === 1);
-        if (bug) bug.fixed = true;
-      })
-    );*/
-    //setGame({ ...game, player: { ...game.player, name: "Bob" } });
-    setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
-    setCart({
-      ...cart,
-      items: cart.items.map((item) =>
-        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
-      ),
+    userService.delete(user.id).catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
     });
   };
 
-  /*<ListGroup
-        heading="Miami"
-        items={items}
-        onSelectItem={() => console.log("clicked")}
-      />
-      {bugs.map((bug) => (
-        <p key={bug.id}>
-          {bug.title} {bug.fixed ? "Fixed" : "New"}
-        </p>
-      ))}
-      <Button color="primary" onClick={handleClick}>
-        Click me
-      </Button> 
-      
-      
-      <NavBar cartItemsCount={cartItems.length} />
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
-      */
+  const addUser = () => {
+    const originalUsers = [...users];
+    const newUser = { id: 0, name: "Mosh" };
+    setUsers([newUser, ...users]);
+
+    userService
+      .create(newUser)
+      .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
+  const updateUser = (user: User) => {
+    const originalUsers = [...users];
+    const updatedUser = { ...user, name: user.name + "!" };
+    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+
+    userService.update(user).catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
+  };
 
   return (
     <div>
-      <Form />
+      <p className="text-danger">{error}</p>
+      {isLoading && <div className="spinner-border"></div>}
+      <button className="btn btn-primary mb-3" onClick={addUser}>
+        Add
+      </button>
+      <ul className="list-group">
+        {users.map((user) => (
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.name}
+            <div>
+              <button
+                className="btn btn-outline-secondary mx-1"
+                onClick={() => updateUser(user)}
+              >
+                Update
+              </button>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => deleteUser(user)}
+              >
+                Delete
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
