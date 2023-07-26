e6d2f8fb7c93637a114ecd2f0ef5f684df9f3f07
David Pilat <dpilat13@gmail.com>
2023-07-25 22:18:25 -0700

    Display Genres Side panel with loading spinner

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -2,17 +2,21 @@ import { Grid, GridItem, Show } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
+import { useState } from "react";
 
 function App() {
+  const [selectedGenre, setSelectedGenre] = useState(null);
+
   return (
     <Grid
       templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" " aside main"` }}
+      templateColumns={{ base: "1fr", lg: "200px 1fr" }}
     >
       <GridItem area="nav">
         <Navbar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">
+        <GridItem area="aside" paddingX={5}>
           <GenreList />
         </GridItem>
       </Show>
