274ddc979d3cbe095a21a88651c19cf632e81d2e
David Pilat <dpilat13@gmail.com>
2023-07-25 20:55:34 -0700

    Show Loading Skeletons

--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -23,22 +23,28 @@ import apiClient, { CanceledError } from "../services/api-client";
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
+    const [isLoading, setLoading] = useState(false);
 
     useEffect(() => {
         const controller = new AbortController();
 
+        setLoading(true);
         apiClient
             .get<FetchGamesResponse>("/games", {signal: controller.signal})
-            .then((res) => setGames(res.data.results))
+            .then((res) => {
+                setGames(res.data.results);
+                setLoading(false); // In dev enviroment you want to do it here other wise put this in the .finally()
+            })
             .catch((err) => {
                 if(err instanceof CanceledError) return;
                 setError(err.message);
+                setLoading(false); // In dev enviroment you want to do it here other wise put this in the .finally()
             });
         
         return () => controller.abort();
     }, []);
 
-    return {games,error};
+    return {games, error, isLoading};
 }
 
 export default useGames;
