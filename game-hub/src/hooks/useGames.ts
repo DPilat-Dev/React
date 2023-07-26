c6d94433aa016fdbb298fbac0b2ad1f55b94a367
David Pilat <dpilat13@gmail.com>
2023-07-25 20:33:33 -0700

    Displaying Critic Score

--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -12,6 +12,7 @@ import apiClient, { CanceledError } from "../services/api-client";
         name: string;
         background_image: string;
         parent_platforms: {platform: Platform}[];
+        metacritic: number;
     }
   
     interface FetchGamesResponse {
