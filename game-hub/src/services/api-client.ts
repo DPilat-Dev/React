e8e147be98ba2eb954302ecb11fca9aef0f07c83
David Pilat <dpilat13@gmail.com>
2023-09-26 14:57:17 -0700

    Build platform selector

--- game-hub/src/services/api-client.ts
+++ game-hub/src/services/api-client.ts
@@ -3,7 +3,7 @@ import axios, { CanceledError } from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: 'Test'
+        key: ''
     }
 });
 
