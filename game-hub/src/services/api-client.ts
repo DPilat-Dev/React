740149a90eaf0ba574d10cedc2e63d60d461e546
David Pilat <dpilat13@gmail.com>
2023-09-26 18:38:00 -0700

    save

--- game-hub/src/services/api-client.ts
+++ game-hub/src/services/api-client.ts
@@ -3,7 +3,7 @@ import axios, { CanceledError } from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: ''
+        key: '3f56599caa604e8eafe04c96491104de'
     }
 });
 
